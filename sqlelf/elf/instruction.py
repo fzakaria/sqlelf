87a85b5e50bdb2d9ffce8818ce55b9775d12031e
Farid Zakaria <fmzakari@google.com>
2023-04-16 15:04:35 +0000

    feat: add elf_instructions table
    This adds a new table 'elf_instructions' that has the
    shared object instructions in all sections set with the
    appropriate flag.
    
    It uses the Capstone library for the disassembly and comparing it
    to objdump looks to be correct:
    
    $ sqlelf /usr/bin/ruby --sql "select mnemonic || ' ' || operands from elf_instructions where section = '.text'" | head
    sub rsp, 0x28
    mov dword ptr [rsp + 0xc], edi
    xor edi, edi
    mov qword ptr [rsp], rsi
    lea rsi, [rip + 0xf3f]
    mov rax, qword ptr fs:[0x28]
    mov qword ptr [rsp + 0x18], rax
    xor eax, eax
    call 0x1090
    mov rsi, rsp
    
    $ objdump -S -d /usr/bin/ruby | grep .text -A 10
    Disassembly of section .text:
    
    00000000000010b0 <_start@@Base-0x80>:
        10b0:	48 83 ec 28          	sub    $0x28,%rsp
        10b4:	89 7c 24 0c          	mov    %edi,0xc(%rsp)
        10b8:	31 ff                	xor    %edi,%edi
        10ba:	48 89 34 24          	mov    %rsi,(%rsp)
        10be:	48 8d 35 3f 0f 00 00 	lea    0xf3f(%rip),%rsi        # 2004 <_IO_stdin_used@@Base+0x4>
        10c5:	64 48 8b 04 25 28 00 	mov    %fs:0x28,%rax
        10cc:	00 00
        10ce:	48 89 44 24 18       	mov    %rax,0x18(%rsp)

---
+++ sqlelf/elf/instruction.py
@@ -0,0 +1,53 @@
+# Without this Python was complaining
+from __future__ import annotations
+
+from typing import Any, Iterator
+
+import apsw
+import apsw.ext
+import capstone
+import lief
+
+
+def elf_instructions(binaries: list[lief.Binary]):
+    def generator() -> Iterator[dict[str, Any]]:
+        for binary in binaries:
+            for section in binary.sections:
+                if section.has(lief.ELF.SECTION_FLAGS.EXECINSTR):
+                    data = bytes(section.content)
+                    md = capstone.Cs(arch(binary), mode(binary))
+                    # keep in mind that producing details costs more memory,
+                    # complicates the internal operations and slows down
+                    # the engine a bit, so only do that if needed.
+                    md.detail = True
+                    for instruction in md.disasm(data, section.virtual_address):
+                        yield {
+                            "path": binary.name,
+                            "section": section.name,
+                            "mnemonic": instruction.mnemonic,
+                            "address": instruction.address,
+                            "operands": instruction.op_str,
+                        }
+
+    return generator
+
+
+def mode(binary: lief.Binary) -> int:
+    if binary.header.identity_class == lief.ELF.ELF_CLASS.CLASS64:
+        return capstone.CS_MODE_64
+    raise Exception(f"Unknown mode for {binary.name}")
+
+
+def arch(binary: lief.Binary) -> int:
+    if binary.header.machine_type == lief.ELF.ARCH.x86_64:
+        return capstone.CS_ARCH_X86
+    raise Exception(f"Unknown machine type for {binary.name}")
+
+
+def register(connection: apsw.Connection, binaries: list[lief.Binary]):
+    generator = elf_instructions(binaries)
+    # setup columns and access by providing an example of the first entry returned
+    generator.columns, generator.column_access = apsw.ext.get_column_names(
+        next(generator())
+    )
+    apsw.ext.make_virtual_module(connection, "elf_instructions", generator)
