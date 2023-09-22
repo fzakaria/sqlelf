eabc3fb53b64a17da7bb7c71d2be42b74c45ccbf
Farid Zakaria <fmzakari@google.com>
2023-09-22 21:52:44 +0000

    Publish to PyPi workflow
    Made the version dynamic using setuptools_scm and publish to PyPi using
    a github action and "trusted publishers"

--- sqlelf/__init__.py
+++ sqlelf/__init__.py
@@ -1,4 +1,11 @@
 import apsw.bestpractice
+from importlib.metadata import version, PackageNotFoundError
 
 # forward sqlite logs to logging module
 apsw.bestpractice.apply(apsw.bestpractice.recommended)
+
+try:
+    __version__ = version("sqlelf")
+except PackageNotFoundError:
+    # If the package is not installed, don't add __version__
+    pass
